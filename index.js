93f129226ea70aa7ec61aca2037044e9772aae7b
hesawmgh1098 <72912744+hesawmgh1098@users.noreply.github.com>
2021-05-21 02:24:18 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const ytdl = require("ytdl-core")
+const {
+    token,
+    channel_id,
+    video_urls
+} = require("./config.json")
+const client = new Discord.Client()
+
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+    const voiceChannel = client.channels.cache.get(channel_id)
+    voiceChannel.join().then(connection => {
+        console.log("Joined voice channel")
+        function play(connection) {
+            const stream = ytdl(video_urls[Math.floor(Math.random() * video_urls.length)], { filter: "audioonly" })
+            const dispatcher = connection.play(stream)
+            dispatcher.on("finish", () => {
+                play(connection)
+            })
+        }
+
+        play(connection)
+    })
+})
+
+client.login(TOKEN)
